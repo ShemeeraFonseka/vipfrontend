4107b411530c279c4cf977374a2bd218ddccc54f
Shemeera Fonseka <shemeerafonseka@gmail.com>
2026-02-02 09:57:41 +0530

    vip frontend

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect , useCallback} from 'react';
 import './Testimonials.css';
 import { FaQuoteLeft } from 'react-icons/fa';
 import axios from 'axios';
